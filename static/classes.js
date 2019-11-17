734373ad05973cc6ded588d1300564cde94f5791
Shaunak Warty <37036096+hyperXcube@users.noreply.github.com>
2019-11-16 21:57:48 -0800

    Request course data from Canvas API

--- static/classes.js
+++ static/classes.js
@@ -1,42 +1,48 @@
-const d = new Date(0); // TODO: Remove 0 when done debugging
 const main = document.getElementById("main");
 
-const classes = ["No classes today!", "APCS", "English 2", "PE", "Precalculus H", "History", "Chemistry H", "Spanish 2"];
-
-let today;
-switch (d.getDay()) {
-    case 1:
-        today = [1, 2, 3, 4, 5, 6];
-        break;
-    case 2:
-        today = [1, 7, 2, 3, 4, 5];
-        break;
-    case 3:
-        today = [1, 6, 7, 2, 3];
-        break;
-    case 4:
-        today = [1, 4, 5, 6, 7, 2];
-        break;
-    case 5:
-        today = [3, 4, 5, 6, 7];
-        break;
-    default:
-        today = [0]
-}
-
-for (let i of today) {
-    const p = document.createElement("p");
-    p.className = "class";
-    p.innerHTML = classes[i];
-    main.appendChild(p);
-}
-
-// HTTP request test - for later
-const xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-        console.log(JSON.parse(xhttp.responseText)["main"]);
+// Canvas API doesn't return all courses for some reason
+canvasRequest("courses?enrollment_state=active", function (response) {
+    const json = JSON.parse(response);
+    for (const c of json) {
+        const date = new Date(c["start_at"]);
+        if (inSchoolYear(date)) {
+            const a = document.createElement("a");
+            a.href = `https://bcp.instructure.com/courses/${c["id"]}`;
+            a.target = "_blank";
+            a.style.display = "block";
+            a.style.margin = "10px 0";
+            a.innerHTML = c["name"];
+            main.appendChild(a);
+        }
     }
-};
-xhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=San%20Jose,us&appid=b63129c5f1096dc67104117a85613732", true);
-xhttp.send();
+});
+
+// Makes HTTP request to Canvas API
+function canvasRequest(apiSection, completion) {
+    const xhttp = new XMLHttpRequest();
+    const corsAnywhere = "https://cors-anywhere.herokuapp.com";
+    const url = `https://bcp.instructure.com/api/v1/${apiSection}`;
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(JSON.parse(xhttp.responseText));
+            completion(xhttp.responseText);
+        }
+    };
+    xhttp.open("GET", `${corsAnywhere}/${url}`, true);
+    xhttp.setRequestHeader("Authorization", `Bearer ${getToken()}`);
+    xhttp.send();
+}
+
+// TODO: get OAuth access token
+function getToken() {
+    return "" // Put your access token here
+}
+
+// Checks if given date is in current school year
+// Note: dates are offset by 5 months so dates in the same school year have the same year ending
+const current = new Date();
+current.setMonth(current.getMonth() - 5);
+function inSchoolYear(date) {
+    date.setMonth(date.getMonth() - 5);
+    return date.getFullYear() === current.getFullYear();
+}
