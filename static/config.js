c71997af8eb5e9e785be65bf076434b3b87ed6e7
Shaunak Warty <37036096+hyperXcube@users.noreply.github.com>
2019-11-18 08:38:48 -0800

    Add config

---
+++ static/config.js
@@ -0,0 +1,5 @@
+const canvasAccessToken = ""; // This can be found in the settings for the canvas account
+
+const clientId = "8866edb7-8c56-48d6-a7cc-223fd09280ea";
+
+const tenantId = "b0e91a46-079b-4108-bd1a-e246d5d2f971";
