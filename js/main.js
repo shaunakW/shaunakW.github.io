9ab3d65d3c49fb20058167fdac74db747fe4b9d3
Shaunak Warty <37036096+hyperXcube@users.noreply.github.com>
2019-11-21 23:44:09 -0800

    Fix issue of drop down links still active when hidden

--- js/main.js
+++ js/main.js
@@ -7,15 +7,22 @@ const dropDown = document.getElementById('nav-drop-down');
 const dropDownArea = document.getElementById('drop-down-area');
 const dropDownImage = document.getElementById('drop-down-image');
 
+dropDown.addEventListener('transitionend', function() {
+    if (!navExpanded) {
+        dropDown.style.display = 'none';
+    }
+});
+
 dropDownArea.onclick = function () {
+    navExpanded = !navExpanded;
     if (navExpanded) {
+        dropDownImage.style.transform = 'rotate(90deg)';
+        dropDown.style.opacity = '1';
+        dropDown.style.display = 'flex';
+        topMenu.style.height = topMenu.clientHeight + dropDown.clientHeight + 'px';
+    } else {
         dropDown.style.opacity = '0';
         topMenu.style.height = '80px';
         dropDownImage.style.transform = 'rotate(0deg)';
-    } else {
-        dropDownImage.style.transform = 'rotate(90deg)';
-        dropDown.style.opacity = '1';
-        topMenu.style.height = topMenu.clientHeight + dropDown.clientHeight + 'px';
     }
-    navExpanded = !navExpanded;
 };
