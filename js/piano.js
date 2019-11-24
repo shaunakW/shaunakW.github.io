415772169c9dd9775d33f4dda7fc5fad4c1ca275
Shaunak Warty <37036096+hyperXcube@users.noreply.github.com>
2019-11-24 15:36:28 -0800

    Recognize key presses

--- js/piano.js
+++ js/piano.js
@@ -1,13 +1,29 @@
 anime({
     targets: '#octave-1 .white-key',
     translateY: [-800, 0],
-    delay: (el, i) => 200 * (i + 1),
+    delay: anime.stagger(200, {start: 200}),
     easing: 'easeOutBack'
 });
 
 anime({
     targets: '#octave-1 .black-key',
     translateY: [-500, 0],
-    delay: (el, i) => 200 * (i + 6),
-    easing: 'easeOutSine'
+    delay: anime.stagger(200, {start: 1200}),
+    easing: 'easeOutBack',
+    complete: readyKeys
 });
+
+function readyKeys() {
+    document.querySelectorAll('.white-key, .black-key').forEach((key, i) => {
+        key.onclick = () => {
+            anime({
+                targets: key,
+                translateY: [0, 25],
+                translateX: [0, -3],
+                duration: 100,
+                easing: 'easeOutSine',
+                direction: 'alternate'
+            });
+        }
+    });
+}
