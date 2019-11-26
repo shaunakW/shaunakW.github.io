531164fc34cd5798a2d4e408f51d158289edbcf7
Shaunak Warty <37036096+hyperXcube@users.noreply.github.com>
2019-11-25 19:25:19 -0800

    Add granim.js gradient to nav bar

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ dropDownButton.onclick = function () {
             anime({
                 targets: '#drop-down',
                 opacity: 1,
-                duration: 600,
+                duration: 500,
                 easing: 'linear'
             });
             anime({
@@ -26,33 +26,52 @@ dropDownButton.onclick = function () {
                 delay: anime.stagger(75)
             });
         }, 0);
-        anime({
-            targets: '#top-menu',
-            height: [80, 80 + dropDown.clientHeight],
-            duration: 1500
-        });
+        document.getElementById('top-gradient').style.height = 80 + dropDown.clientHeight + 'px';
+        // anime({
+        //     targets: '#top-gradient',
+        //     height: 80 + dropDown.clientHeight,
+        //     duration: 1500
+        // });
     } else {
         anime({
             targets: '#drop-down',
             opacity: 0,
-            duration: 200,
+            duration: 125,
             easing: 'linear'
         });
         anime({
             targets: '#drop-down *',
             translateY: anime.stagger(-10, {start: -50}),
-            duration: 500,
+            duration: 450,
             easing: 'easeOutCubic',
             complete: () => dropDown.style.display = 'none'
         });
-        anime({
-            targets: '#top-menu',
-            height: [80 + dropDown.clientHeight, 80],
-            duration: 1500
-        });
+        // anime({
+        //     targets: '#top-gradient',
+        //     height: 80,
+        //     duration: 1500
+        // });
+        document.getElementById('top-gradient').style.height = 80 + 'px';
         anime({
             targets: '#drop-down-image',
             rotate: 0
         });
     }
 };
+
+new Granim({
+    element: '#top-gradient',
+    direction: 'left-right',
+    isPausedWhenNotInView: true,
+    states : {
+        'default-state': {
+            gradients: [
+                ['#0abde3', '#2ecc71'],
+                ['#2ecc71', '#3498db'],
+                ['#3498db', '#9b59b6'],
+                ['#9b59b6', '#ee5253'],
+                ['#ee5253', '#54a0ff']
+            ]
+        }
+    }
+});
